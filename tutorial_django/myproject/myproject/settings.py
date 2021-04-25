b62659ba49d45cb6820d6150c999310d5da3384f
Yulia <jalbova@gmail.com>
2021-04-25 16:32:13 +0100

    use db from devel

--- tutorial_django/myproject/myproject/settings.py
+++ tutorial_django/myproject/myproject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': '/home/yulia/devel/db.sqlite3',
     }
 }
 
