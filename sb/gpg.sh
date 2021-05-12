2ec735ef9af248a34b354f5fe56b012c910353b7
dimanne <dimanne@gmail.com>
2021-05-12 19:28:14 +0100

    Add gpg

---
+++ sb/gpg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+while [[ $# -gt 0 ]]
+   do
+      key="$1"
+      case $key in
+      --import)
+         mkdir $GPGH
+         echo "secret key" >> $GPGH/key
+         exit 0
+      ;;
+      --sign)
+         FILENAME="$2"; shift; shift;
+         cat $GPGH/key >> $FILENAME.sig
+         echo $(md5sum $FILENAME) >> $FILENAME.sig
+      ;;
+      *)    # unknown option
+         echo "unknown option: $key"
+         exit 1
+      ;;
+   esac
+done
