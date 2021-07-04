185e8f7527dc1ae494356e8dffea838918e48106
Yulia <jalbova@gmail.com>
2021-07-04 21:16:12 +0100

    add topics

--- my-site-django/myproject/boards/urls.py
+++ my-site-django/myproject/boards/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls import url
+
 urlpatterns = [
     path('', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.render_topics_of_board, name='board_topics'),
 ]
--- my-site-django/myproject/boards/views.py
+++ my-site-django/myproject/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # from typing import List, Set, Tuple
 # import typing
@@ -6,9 +6,13 @@ import typing as t
 
 # Create your views here.
 
-from django.shortcuts import render
-from .models import Board
+import django.shortcuts as helpers
+from . import models as mdl
 
 def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+    boards = mdl.Board.objects.all()
+    return helpers.render(request, 'home.html', {'boards': boards})
+
+def render_topics_of_board(request, pk):
+    # helpers.get_object_or_404
+    return helpers.render(request, 'topics.html', {'board': board})
